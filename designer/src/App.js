f903c0417ee010daf4b2cc92595b7d2c1f32f083
ElianaC <eliana.gl.c.c@gmail.com>
2022-06-30 13:51:53 -0500

    feat: comunicate react and angular

--- designer/src/App.js
+++ designer/src/App.js
@@ -1,13 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { useEffect, useState } from "react";
 
 function App() {
+  const [count, setCount] = useState(0);
+
+  useEffect(() => {
+    window.addEventListener("addToCart", (event) => {
+      setCount((count) => count + 1);
+      console.log("I am listening this", event.detail);
+    });
+    return () => {
+      window.removeEventListener("addToCart", null);
+    };
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
+          {count}
         </p>
         <a
           className="App-link"
